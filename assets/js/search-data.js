afa1d810e79e1cdce66525036c15502eddbe5e79
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2026-03-03 12:23:31 +0000

    Deploying to gh-pages from @ ivanbrillo/ivanbrillo.github.io@77c77ba3da456d12a4dd00110e2b92e6bbb16b5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,6 +421,11 @@ ninja.data = [{
           description: "Open notes from the MSc in AI and Data Engineering at the University of Pisa",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-domain-adversarial-transformer-for-r-peak-detection-from-eeg",
+          title: 'Domain-Adversarial Transformer for R-Peak Detection from EEG',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
