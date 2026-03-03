2949131b539b195c0885aa36e670b20c57532c6b
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2026-03-03 08:44:42 +0000

    Deploying to gh-pages from @ ivanbrillo/ivanbrillo.github.io@9a4fcd7214346e351d69f381c438fda44d62fc4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,35 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("mailto:%62%72%69%6C%6C%6F%69%76%61%6E%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/ivan-brillo/", "_blank");
         },
       },{
       id: 'light-theme',
