b584cc3d8fefe5fe98346f2c3ab446fffc4961a6
ivanbrillo <117514607+ivanbrillo@users.noreply.github.com>
2026-03-03 12:27:44 +0000

    Deploying to gh-pages from @ ivanbrillo/ivanbrillo.github.io@5757202f337571485ac7fe86ff9f121259ecdff6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,8 +421,8 @@ ninja.data = [{
           description: "Open notes from the MSc in AI and Data Engineering at the University of Pisa",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-domain-adversarial-transformer-for-r-peak-detection-from-eeg",
-          title: 'Domain-Adversarial Transformer for R-Peak Detection from EEG',
+            },},{id: "projects-domain-adversarial-transformer-for-r-peak-detection",
+          title: 'Domain-Adversarial Transformer for R-Peak Detection',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
